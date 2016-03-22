06c7ec1f8e676973d165f585eb1e3d646072f852
Robert Strandh <robert.strandh@gmail.com>
2016-03-22 08:34:01 +0100

    Explain how to interpret the slots of a MATRIX-MASK.

--- Code/mask.lisp
+++ Code/mask.lisp
@@ -3,6 +3,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Class MATRIX-MASK.
+;;;
+;;; The MATRIX slot contains a 2-dimensional array of DOUBLE-FLOAT
+;;; opacity values.  Typically, there will be no row and no column in
+;;; that array with all opacity values being zero, but that is not a
+;;; requirement.
+;;;
+;;; The slots DX and DY determine the position of the array in the
+;;; plane.  The opacity value of the element determined by the form
+;;; (AREF MATRIX Y X) should be interpreted as the opacity value of
+;;; the pixel at position <(+ X DX),(+ Y DY)>.
 
 (defclass matrix-mask (mask)
   ((%matrix :initarg :matrix :reader matrix)
