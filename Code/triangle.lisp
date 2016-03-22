2e733da59cf3b1c2b84cff48eed891d272988fc3
Robert Strandh <robert.strandh@gmail.com>
2016-03-22 08:20:37 +0100

    Remove code for triangle area.  It is no longer needed.

--- Code/triangle.lisp
+++ Code/triangle.lisp
@@ -7,14 +7,3 @@
    (%y2 :initarg :y2 :reader y2)
    (%x3 :initarg :x3 :reader x3)
    (%y3 :initarg :y3 :reader y3)))
-
-(defun area-of-triangle (triangle)
-  (let ((x1 (x1 triangle))
-	(y1 (y1 triangle))
-	(x2 (x2 triangle))
-	(y2 (y2 triangle))
-	(x3 (x3 triangle))
-	(y3 (y3 triangle)))
-    (* 0.5d0
-       (abs (- (* (- x2 x1) (- y3 y1))
-	       (* (- x3 x1) (- y2 y1)))))))
