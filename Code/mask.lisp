fb28052b4784322eac4d32baec4e711c8a87d7a8
Robert Strandh <robert.strandh@gmail.com>
2016-03-22 09:13:08 +0100

    New function for computing the extreme points of a set of triangles in a triangle mask.

--- Code/mask.lisp
+++ Code/mask.lisp
@@ -32,3 +32,28 @@
 
 (defclass triangle-mask (mask)
   ((%triangles :initarg :triangles :reader triangles)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Turn an instance of TRIANGLE-MASK into an instance of MATRIX-MASK.
+
+;;; Return the extreme points of the triangles in the mask as four
+;;; values: MIN-X, MIN-Y, MAX-X, and MAX-Y.
+;;;
+;;; This function assumes that there is at least one triangle in the
+;;; list of triangles in TRIANGLE-MASK.
+(defun compute-extremes (triangle-mask)
+  (loop for triangle in (triangles triangle-mask)
+	minimize (x1 triangle) into min-x
+	minimize (x2 triangle) into min-x
+	minimize (x3 triangle) into min-x
+	maximize (x1 triangle) into max-x
+	maximize (x2 triangle) into max-x
+	maximize (x3 triangle) into max-x
+	minimize (y1 triangle) into min-y
+	minimize (y2 triangle) into min-y
+	minimize (y3 triangle) into min-y
+	maximize (y1 triangle) into max-y
+	maximize (y2 triangle) into max-y
+	maximize (y3 triangle) into max-y
+	finally (return (values min-x min-y max-x max-y))))
