f2b61a7651acee8ef3b5b2558b699d8dbb1da047
Robert Strandh <robert.strandh@gmail.com>
2016-03-22 08:27:07 +0100

    Divide the contents using comments.

--- Code/mask.lisp
+++ Code/mask.lisp
@@ -1,12 +1,24 @@
 (cl:in-package #:cloak)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class MATRIX-MASK.
+
 (defclass matrix-mask (mask)
   ((%matrix :initarg :matrix :reader matrix)
    (%dx :initarg :dx :reader dx)
    (%dy :initarg :dy :reader dy)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class COMPUTED-MASK.
+
 (defclass computed-mask (mask)
   ((%function :initarg :function :reader function)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class TRIANGLE-MASK.
 
 (defclass triangle-mask (mask)
   ((%triangles :initarg :triangles :reader triangles)))
